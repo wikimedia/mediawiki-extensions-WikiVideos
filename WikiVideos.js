7398f35c21543881a55fc1986226eb92101ce5f3
Zoran Dori <zorandori4444@gmail.com>
2023-01-05 16:00:21 +0100

    build: Update versions of eslint related npm packages + add stylelint
    Change-Id: I42a2298ad0782bc03b71a437d4a5a3daac446810

--- WikiVideos.js
+++ WikiVideos.js
@@ -8,7 +8,7 @@ WikiVideos = {
 		var $link = $( this );
 		var $video = $link.closest( '.wikivideo-chapters' ).prev( 'video' );
 		var seconds = $link.data( 'seconds' );
-		$video[0].currentTime = seconds;
+		$video[ 0 ].currentTime = seconds;
 	}
 };
 
